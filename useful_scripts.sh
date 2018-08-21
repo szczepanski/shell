dc629cf7ca2a23b51b7b4d90080f3d83c3512697
Piotr <ask.piotr@yahoo.co.uk>
2018-08-21 10:42:10 +0100

    Update useful_scripts.sh

--- useful_scripts.sh
+++ useful_scripts.sh
@@ -1,4 +1,8 @@
 # MAILX 
 ````
-echo test | mailx -a /apps/home/ipautotsystemsbpprod/Marco.docx -s test piotr.szczepanski@ipsoft.com
+echo test | mailx -a /apps/home/FileToSend.docx -s test piotr@myemail.com
+```
+
+```shell
+echo -e "Hi, \n\nCisco ASA firewall report  attached\n\nKind Regards\n\nIPautomata" | mailx -a /apps/home/ipautotsystemsbpprod/cisco_asa_report_directory/cisco_asa_report.docx -s "TSystems Cisco Firewall Report" piotr.szczepanski@ipsoft.com
 ```
